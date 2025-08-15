c7f2decfb3e0975b8c2bb0ba4d9c0c71efc3e82d
Xaeky <gh.sktd@protonmail.com>
2025-08-14 23:56:08 -0300

    api: add cache to endpoint

--- server/api/public/content/[...filename].get.ts
+++ server/api/public/content/[...filename].get.ts
@@ -1,6 +1,8 @@
-export default defineEventHandler(async (event) => {
+export default defineCachedEventHandler(async (event) => {
   const filename = (getRouterParams(event)).filename;
   const data = await useFetchRemoteContent(event, filename);
   setResponseHeader(event, 'content-type', 'text/markdown');
   return data;
+}, {
+  maxAge: 60 * 60,
 });
